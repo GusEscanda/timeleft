ad81779bef612f7756fbab1b2c78417537af3f70
GusEscanda <59749153+GusEscanda@users.noreply.github.com>
2025-12-16 16:03:28 -0300

    Update service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,53 @@
-const CACHE_NAME = "process-estimator-v1";
-const ASSETS = [
+const CACHE_NAME = "timeleft-v1";
+
+const STATIC_ASSETS = [
     "./",
     "./index.html",
     "./app.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
+// ---- Install ----
 self.addEventListener("install", (event) => {
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
+            return cache.addAll(STATIC_ASSETS);
         })
     );
 });
 
+// ---- Activate ----
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+// ---- Fetch ----
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
+        fetch(event.request)
+            .then((networkResponse) => {
+                const responseClone = networkResponse.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+
+                return networkResponse;
+            })
+            .catch(() => caches.match(event.request))
     );
 });
